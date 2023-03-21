cce3fe7f7c009c228899a9a825e2f1f2c8b4a670
bkrisztian01 <bui.krisztian01@gmail.com>
2023-03-21 20:12:27 +0100

    CreateListing almost done

--- frontend/src/components/CreateListing.tsx
+++ frontend/src/components/CreateListing.tsx
@@ -1,10 +1,162 @@
-import { Heading } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  GridItem,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  SimpleGrid,
+  Textarea,
+} from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import FileUpload from './FileUpload';
+
+type FormInput = {
+  title: string;
+  description: string;
+  address: string;
+  price: number;
+  roomCount: number;
+  area: number;
+  image: FileList;
+};
+
+const createListingSchema = yup.object<FormInput>({
+  title: yup.string().required(),
+  description: yup.string().required(),
+  address: yup.string().required(),
+  price: yup.number().required(),
+  roomCount: yup.number().required(),
+  area: yup.number().required(),
+  // image: yup.string().required(),
+});
+
+// const style = {
+//   '.chakra-form-control': {
+//     marginTop: 3,
+//   },
+//   '.chakra-form-control:first-child': {
+//     marginTop: 0,
+//   },
+// };
 
 const CreatingListing = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<FormInput>({
+    resolver: yupResolver(createListingSchema),
+  });
+
+  // const { mutate, isLoading, isError, error } = useMutation<
+  //   unknown,
+  //   AxiosError
+  // >((data) => {
+  //   return data;
+  // });
+
+  const onSubmit: SubmitHandler<FormInput> = (data: FormInput) => {
+    alert('idk');
+  };
+
   return (
-    <Heading as="h2" size="md">
-      Create listing
-    </Heading>
+    <>
+      <Container maxW="container.sm" py="10px">
+        <Heading as="h1" mb="4">
+          Create listing
+        </Heading>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <SimpleGrid columns={{ base: 2, sm: 1 }} columnGap={2} rowGap={2}>
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Title</FormLabel>
+                <Input {...register('title')} />
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Description</FormLabel>
+                <Textarea {...register('description')} size="sm" rounded="md" />
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Address</FormLabel>
+                <Input {...register('address')} />
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Price</FormLabel>
+                <Input {...register('price')} />
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>Room count</FormLabel>
+                <NumberInput min={1}>
+                  <NumberInputField {...register('roomCount')} />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={1}>
+              <FormControl>
+                <FormLabel>Area</FormLabel>
+                <InputGroup>
+                  <NumberInput max={50} min={1}>
+                    <NumberInputField {...register('area')} />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                  <InputRightAddon>
+                    m<sup>2</sup>
+                  </InputRightAddon>
+                </InputGroup>
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Image</FormLabel>
+                <FileUpload
+                  register={register('image')}
+                  accept="image/*"
+                ></FileUpload>
+              </FormControl>
+            </GridItem>
+
+            <GridItem colSpan={2} mt="4">
+              <Button type="submit" w="100%" colorScheme="green">
+                Submit
+              </Button>
+            </GridItem>
+          </SimpleGrid>
+        </form>
+      </Container>
+    </>
   );
 };
 
---
+++ frontend/src/components/FileUpload.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Icon, InputGroup, Text } from '@chakra-ui/react';
+import { ChangeEvent, ReactNode, useRef, useState } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import { AiOutlineUpload } from 'react-icons/ai';
+
+type FileUploadProps = {
+  register: UseFormRegisterReturn;
+  accept?: string;
+  multiple?: boolean;
+  children?: ReactNode;
+};
+
+const FileUpload = (props: FileUploadProps) => {
+  const { register, accept, multiple, children } = props;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const { ref, onChange, ...rest } = register as {
+    ref: (instance: HTMLInputElement | null) => void;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => Promise<void | boolean>;
+  };
+
+  const [fileName, setFileName] = useState('');
+
+  const handleClick = () => inputRef.current?.click();
+
+  const myOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFileName(e.target.files ? e.target.files[0].name : '');
+    onChange(e);
+  };
+
+  return (
+    <InputGroup onClick={handleClick}>
+      <input
+        type={'file'}
+        multiple={multiple || false}
+        hidden
+        accept={accept}
+        {...rest}
+        onChange={myOnChange}
+        ref={(e) => {
+          ref(e);
+          inputRef.current = e;
+        }}
+      />
+      <>
+        <Button
+          size="md"
+          minWidth="fit-content"
+          leftIcon={<Icon as={AiOutlineUpload} />}
+        >
+          Upload
+        </Button>
+        <Box width="" ml="2" mt="auto" mb="auto" overflowX="hidden">
+          <Text overflow="hidden">{fileName}</Text>
+        </Box>
+      </>
+    </InputGroup>
+  );
+};
+
+export default FileUpload;
